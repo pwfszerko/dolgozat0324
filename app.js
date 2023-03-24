90a4ca922349829b9e2de51aac047271f302aae2
pwfszerko <bendeguzszeri@gmail.com>
2023-03-24 09:40:13 +0100

    masodik

---
+++ app.js
@@ -0,0 +1,25 @@
+//Szeri Bendegúz
+//2023.03.24
+//I/2/N
+
+var AoldalInput = document.querySelector('#Aoldal');
+var BoldalInput = document.querySelector('#Boldal');
+var ColdalInput = document.querySelector('#Coldal');
+var calcButton = document.querySelector('#calcButton');
+var terfogatInput = document.querySelector('#terfogat');
+
+calcButton.addEventListener('click', () => {
+    inputCon();
+});
+
+function inputCon() {
+    let Aoldal = Number(AoldalInput.value);
+    let Boldal = Number(BoldalInput.value);
+    let Coldal = Number(ColdalInput.value);
+
+    let terfogat= calcterfogat(Aoldal, Boldal, Coldal);
+    terfogatInput.value= " " + terfogat;
+
+    function calcterfogat(Aoldal, Boldal, Coldal) {
+        return Aoldal*Boldal*Coldal;
+    }
