bf11f3cadd1f2f0b6f90cafa954474fcbbf64060
pwfszerko <bendeguzszeri@gmail.com>
2023-03-24 10:02:55 +0100

    negyedik

--- scr/scr/app.js
+++ scr/scr/app.js
@@ -17,8 +17,10 @@ function inputCon() {
     let Boldal = Number(BoldalInput.value);
     let Coldal = Number(ColdalInput.value);
 
+
     let terfogat= calcterfogat(Aoldal, Boldal, Coldal);
     terfogatInput.value= " " + terfogat;
+}
 
     function calcterfogat(Aoldal, Boldal, Coldal) {
         return Aoldal*Boldal*Coldal;
